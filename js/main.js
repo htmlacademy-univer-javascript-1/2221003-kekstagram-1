8a12cdadcffccf7837f3b16e7ffe6a4073e67673
Naviprolol <103417013+Naviprolol@users.noreply.github.com>
2022-09-19 10:08:02 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomNumber(min, max) {
+
+const getRandomNumber = (min, max) => {
   if (min < max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-}
+};
 
-function isRightLength(string, maxLength) {
-  if (String(string).length <= maxLength) {
-    return true;
-  }
-  return false;
-}
+const isRightLength = (string, maxLength) => String(string).length <= maxLength;
