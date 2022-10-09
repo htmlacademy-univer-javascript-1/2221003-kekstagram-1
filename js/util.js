bad9e517f965a54db44f92888d49110603dcd1bb
Naviprolol <103417013+Naviprolol@users.noreply.github.com>
2022-10-09 23:16:38 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -5,7 +5,6 @@ const getRandomPositiveInteger = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 };
-
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 export { getRandomPositiveInteger, getRandomArrayElement };
