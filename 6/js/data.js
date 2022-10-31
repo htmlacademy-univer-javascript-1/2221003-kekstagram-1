fe62648db7dbb64e5648283b6f3570ccac844574
Keks <keks@htmlacademy.ru>
2022-10-31 15:48:29 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/data.js
+++ 6/js/data.js
@@ -3,17 +3,17 @@ import { NAMES, MESSAGES, DESCRIPTIONS, MAX_COUNT_PHOTOS } from './constants.js'
 
 const createComment = (id) => ({
   id,
-  avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+  avatar: `img/avatar-${getRandomPositiveInteger(1, MAX_COUNT_PHOTOS)}.svg`,
   message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES),
 });
 
 const createPhotos = (id) => ({
   id,
-  url: `photos/${id + 1}.jpg`,
+  url: `photos/${id}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomPositiveInteger(15, 200),
-  comments: createComment(id)
+  comments: Array.from({ length: getRandomPositiveInteger(1, 6) }).map((value, index) => createComment(index + 1)),
 });
 
 const getPhotos = () => Array.from({ length: MAX_COUNT_PHOTOS }).map((element, index) => { element = createPhotos(index + 1); });
