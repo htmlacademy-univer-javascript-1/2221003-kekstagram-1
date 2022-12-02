ef6d41150f997fb3be1e3c241860f52b34e980da
Vadim Roshchin <Vadim.Roshchin@urfu.me>
2022-12-02 22:51:51 +0500

    Помощь друга

--- js/constants.js
+++ js/constants.js
@@ -33,6 +33,33 @@ const MaxHashtag = {
   COUNT: 5
 };
 
+const Effects = {
+  'original': {
+    name: 'original', filter: '', unit: '',
+    options: { range: { min: 0, max: 100 }, step: 1, start: 100 },
+  },
+  'heat': {
+    name: 'heat', filter: 'brightness', unit: '',
+    options: { range: { min: 1, max: 3 }, step: 0.1, start: 3 },
+  },
+  'chrome': {
+    name: 'chrome', filter: 'grayscale', unit: '',
+    options: { range: { min: 0, max: 1 }, step: 0.1, start: 1 },
+  },
+  'sepia': {
+    name: 'sepia', filter: 'sepia', unit: '',
+    options: { range: { min: 0, max: 1 }, step: 0.1, start: 1 },
+  },
+  'marvin': {
+    name: 'marvin', filter: 'invert', unit: '%',
+    options: { range: { min: 0, max: 100 }, step: 1, start: 100 },
+  },
+  'phobos': {
+    name: 'phobos', filter: 'blur', unit: 'px',
+    options: { range: { min: 0, max: 3 }, step: 0.1, start: 3 },
+  }
+};
+
 const ErrorMessage = {
   HASH_SPACE: 'Хэш-теги разделяются пробелами',
   STARTS_WITH_HASH: 'Хэш-тег начинается с символа #',
@@ -46,5 +73,6 @@ const ErrorMessage = {
 
 export {
   MAX_COUNT_PHOTOS, NAMES, MESSAGES, DESCRIPTIONS, CountLike, AvatarNumber, CountComment,
-  DEFAULT_RENDERED_COMMENTS, STEP_ADDED_COMMENTS, MAX_COMMENT_LENGTH, MaxHashtag, ErrorMessage
+  DEFAULT_RENDERED_COMMENTS, STEP_ADDED_COMMENTS, MAX_COMMENT_LENGTH, MaxHashtag, ErrorMessage,
+  Effects
 };
--- js/user-form.js
+++ js/user-form.js
@@ -1,4 +1,6 @@
 import { onInput, commentHandler, hashtagsHandler, pristine, error } from './validate.js';
+import { changeEffects } from './effects-filter.js';
+import { onClickScaleButtons } from './scale.js';
 
 const file = document.querySelector('#upload-file');
 const body = document.querySelector('body');
@@ -7,14 +9,14 @@ const form = document.querySelector('.img-upload__form');
 const closeButton = form.querySelector('.img-upload__cancel');
 const comments = form.querySelector('.text__description');
 const hashtags = form.querySelector('.text__hashtags');
-
+const imageForChange = document.querySelector('.img-upload__preview_img');
 
 const closePopup = () => {
   imgUpload.classList.add('hidden');
   body.classList.remove('modal-open');
+  document.querySelector('.img-upload__effect-level').classList.add('hidden');
   form.reset();
 };
-
 
 const onButtonEscKeydown = (evt) => {
   if (evt.key === 'Escape') {
@@ -40,12 +42,16 @@ const checkFieldInFocus = (field) => {
 
 
 const onImgUploadFieldchange = () => {
+  imageForChange.removeAttribute('class');
+  imageForChange.removeAttribute('style');
   imgUpload.classList.remove('hidden');
   body.classList.add('modal-open');
   closeButton.addEventListener('click', onCloseButtonClick);
   document.addEventListener('keydown', onButtonEscKeydown);
   checkFieldInFocus(comments);
   checkFieldInFocus(hashtags);
+  changeEffects();
+  onClickScaleButtons();
 };
 
 
