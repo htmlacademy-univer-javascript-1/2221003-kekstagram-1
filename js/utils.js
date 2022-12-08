844c8e34c77e088c5963f267021583d40d30ec2a
Vadim Roshchin <Vadim.Roshchin@urfu.me>
2022-12-09 00:11:08 +0500

    trycolors

--- js/utils.js
+++ js/utils.js
@@ -14,14 +14,17 @@ const checkStringLength = (string, length) => string.length <= length;
 
 const showAlert = (message) => {
   const alertPopup = document.querySelector(`#${message}`).content.querySelector('section').cloneNode(true);
+  const alertTitle = alertPopup.querySelector('h2');
+  const alertButton = alertPopup.querySelector('button');
   alertPopup.style.zIndex = '100';
-  alertPopup.style.left = '0';
-  alertPopup.style.top = '0';
-  alertPopup.style.right = '0';
-  alertPopup.style.padding = '10px 3px';
-  alertPopup.style.fontSize = '30px';
-  alertPopup.style.textAlign = 'center';
-  alertPopup.style.color = 'blue';
+  alertTitle.style.left = '0';
+  alertTitle.style.top = '0';
+  alertTitle.style.right = '0';
+  alertTitle.style.padding = '10px 3px';
+  alertTitle.style.fontSize = '30px';
+  alertTitle.style.textAlign = 'center';
+  alertTitle.style.color = 'yellow';
+  alertButton.style.color = 'red';
   document.body.append(alertPopup);
   const button = alertPopup.querySelector('button');
   button.addEventListener('click', () => {
